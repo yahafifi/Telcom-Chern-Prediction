6c4b633c700f10c4dd38645c8dd13e4a43931023
yahafifi <47195825+yahafifi@users.noreply.github.com>
2025-12-21 23:14:39 +0200

    add flask api

--- app/app.py
+++ app/app.py
@@ -1,37 +1,65 @@
 import joblib
 import pandas as pd
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
 
 def load_model():
     model = joblib.load('C:\\Users\\yahaf\\OneDrive\\Desktop\\New folder\\Telcom-Chern-Prediction\\model\\telco.joblib')
     return model
 
-# Example new customer
-example_customer = pd.DataFrame({
-    "gender": ["Female"],
-    "SeniorCitizen": ["No"],
-    "Partner": ["Yes"],
-    "Dependents": ["Yes"],
-    "tenure": [10],
-    "PhoneService": ["Yes"],
-    "MultipleLines": ["Yes"],
-    "InternetService": ["Fiber optic"],
-    "OnlineSecurity": ["No"],
-    "OnlineBackup": ["No"],
-    "DeviceProtection": ["No"],
-    "TechSupport": ["No"],
-    "StreamingTV": ["Yes"],
-    "StreamingMovies": ["Yes"],
-    "Contract": ["Two year"],
-    "PaperlessBilling": ["Yes"],
-    "PaymentMethod": ["Electronic check"],
-    "MonthlyCharges": [80.0],
-    "TotalCharges": [800.0]
-})
+def prepare_data(data):
+    gender = data.get('gender')
+    SeniorCitizen = data.get('SeniorCitizen')
+    Partner = data.get('Partner')
+    Dependents = data.get('Dependents')
+    tenure = data.get('tenure')
+    PhoneService = data.get('PhoneService')
+    MultipleLines = data.get('MultipleLines')
+    InternetService = data.get('InternetService')
+    OnlineSecurity = data.get('OnlineSecurity')
+    OnlineBackup = data.get('OnlineBackup')
+    DeviceProtection = data.get('DeviceProtection')
+    TechSupport = data.get('TechSupport')
+    StreamingTV = data.get('StreamingTV')
+    StreamingMovies = data.get('StreamingMovies')
+    Contract = data.get('Contract')
+    PaperlessBilling = data.get('PaperlessBilling')
+    PaymentMethod = data.get('PaymentMethod')
+    MonthlyCharges = data.get('MonthlyCharges')
+    TotalCharges = data.get('TotalCharges')
+    example_customer = pd.DataFrame({
+        "gender": [gender],
+        "SeniorCitizen": [SeniorCitizen],
+        "Partner": [Partner],
+        "Dependents": [Dependents],
+        "tenure": [int(tenure)],
+        "PhoneService": [PhoneService],
+        "MultipleLines": [MultipleLines],
+        "InternetService": [InternetService],
+        "OnlineSecurity": [OnlineSecurity],
+        "OnlineBackup": [OnlineBackup],
+        "DeviceProtection": [DeviceProtection],
+        "TechSupport": [TechSupport],
+        "StreamingTV": [StreamingTV],
+        "StreamingMovies": [StreamingMovies],
+        "Contract": [Contract],
+        "PaperlessBilling": [PaperlessBilling],
+        "PaymentMethod": [PaymentMethod],
+        "MonthlyCharges": [float(MonthlyCharges)],
+        "TotalCharges": [float(TotalCharges)]
+    })
+    return example_customer
 
-model = load_model()
-pred =  model.predict(example_customer)[0]
-# print the prediction result
-if pred == 1:
-  print('Churn')
-else:
-  print('Not Churn')
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.form
+    example_customer = prepare_data(data)
+    model = load_model()
+    pred = model.predict(example_customer)[0]
+    if pred == 1:
+      return jsonify({"prediction": "Churn"})
+    else:
+      return jsonify({"prediction": "Not Churn"})
+
+app.run()
